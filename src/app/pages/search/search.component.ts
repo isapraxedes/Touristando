e7664878d4d668a4e469fdb85df15968339b5e9c
Isabelly <bellypraxedes@gmail.com>
2016-11-25 17:44:29 -0300

    Implementação tela de pesquisa

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+
+import { Local } from './../../model/local';
+import { CreateComponent } from './../pages/local/create.component';
+
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'search',
   templateUrl: './search.html'
 })
+
 export class SearchComponent {
-  
+  locals: FirebaseListObservable<Local[]>;
+
+  constructor(private af: AngularFire) {
+
+    this.af.auth.login({ email: 'vl@cin.ufpe.br', password: '123456' });
+
+    this.locals = af.database.list('/locals', {
+    query: {
+        orderByChild: 'city',
+      }
+  });
+  }
+
+  let list: Local[] = [];
  }
